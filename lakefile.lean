bd7411bf4b90d3b8145fe0e1cda2793c4ca0110f
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-27 01:05:08 -0800

    openssl

--- lakefile.lean
+++ lakefile.lean
@@ -9,14 +9,22 @@ require crucible from ".." / "crucible"
 require chronicle from ".." / "chronicle"
 require staple from ".." / "staple"
 
+-- OpenSSL linking (required by citadel's TLS support via loom)
+-- Lake doesn't propagate moreLinkArgs from dependencies, so we must add them here
+def opensslLinkArgs : Array String :=
+  #["-L/opt/homebrew/opt/openssl@3/lib", "-lssl", "-lcrypto"]
+
 @[default_target]
 lean_lib HomebaseApp where
   roots := #[`HomebaseApp]
+  moreLinkArgs := opensslLinkArgs
 
 lean_exe homebaseApp where
   root := `HomebaseApp.Main
+  moreLinkArgs := opensslLinkArgs
 
 -- Test executable
 @[test_driver]
 lean_exe tests where
   root := `Tests.Main
+  moreLinkArgs := opensslLinkArgs
